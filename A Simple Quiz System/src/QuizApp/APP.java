93cf993fa2b40f8fa81e492c69cff8d1c41a0388
ALLY SADDAM n <saddamnassor6@gmail.com>
2020-08-20 03:14:45 +0300

    added a question

--- A Simple Quiz System/src/QuizApp/APP.java
+++ A Simple Quiz System/src/QuizApp/APP.java
@@ -2,21 +2,24 @@ package QuizApp;
 
 import java.util.Scanner;
 
+import QuizApp.Question;
+
 public class APP {
 
 	public static void main(String[] args) {
 		
-		String q1 = "What color are apples?" 
+		String q1 = "What color are apples?\n" 
 					+ "(a)red/green\n(b)orange\n(c)Magenta\n"; 
 		String q2 = "What color are bananas?" 
 					+ "(a)red/green\n(b)yellow\n(c)blue\n"; 
-
+		String q3 = "Who is the president of your country?\n" 
+				+ "(a)red/Kenyatta\n(b)Magufuli\n(c)Trump\n"; 
 		
 		
 	Question [] questions = {	
 			new Question(q1, "a"),
-			new Question(q2, "b")
-			
+			new Question(q2, "b"),
+			new Question(q3, "b")
 			};
 			takeTest(questions);
 	}
